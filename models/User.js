14b1fbd07bd0c40564b8a4c64643ecf396111e27
Eric Kohler <erickohler1600@gmail.com>
2022-07-16 16:37:47 -0400

    setup user and thought models/schema

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      match: /.+\@.+\..+/,
+      unique: true
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+);
+
+// get lenght of friends array
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
