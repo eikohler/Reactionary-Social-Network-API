e356df91dfd27eef767cd9a83899009276f632b2
Eric Kohler <erickohler1600@gmail.com>
2022-07-16 16:05:29 -0400

    Finish routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,13 +10,13 @@ const {
     removeFriend
   } = require('../../controllers/user-controller');
 
-// Set up GET all and POST at /api/pizzas
+
 router
   .route('/')
   .get(getAllUsers)
   .post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+
 router
   .route('/:id')
   .get(getUserbyID)
